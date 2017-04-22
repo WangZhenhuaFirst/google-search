b9c8b9706339fc174347504c9496f3f66cb65ed0
huazai <470021389@qq.com>
2017-04-22 21:50:21 +0800

    implement major function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,8 @@ class SearchController < ApplicationController
 
   def index
     if params[:q]
-      page = params[:page] || 1
-      @results = GoogleCustomSearchApi.search(params[:q],
-                                              page: page)
+      @results = GoogleCustomSearchApi.search(params[:q])
+      
 	  end
   end
 
